510af27e3da6977b8d235c2c5f897d50de2f4b47
milan <patelmilang@gmail.com>
2025-01-15 21:47:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+ 
+import './App.css';
+import EmployeeList from './EmployeeList';
+
+function App() {
+
+
+  
+
+  return (
+    <div>
+      <EmployeeList /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/EmployeeList.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+
+
+const EmployeeList = () => {
+  const [data, setData] = useState([]);  
+  const [currentPage, setCurrentPage] = useState(1);
+  const recordsPerPage = 10;
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+      );
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+   
+  useEffect(() => {
+    
+
+    fetchData();
+  }, []);
+
+  
+  const lastIndex = currentPage * recordsPerPage;
+  const firstIndex = lastIndex - recordsPerPage;
+  const currentRecords = data.slice(firstIndex, lastIndex);
+  const totalPages = Math.ceil(data.length / recordsPerPage);
+  
+  const handlePrev = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+  };
+
+  return (
+    <div className="table-container">
+      <h2 className="table-title">Employee Data Table</h2>
+      <table className="employee-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentRecords.length > 0 ? (
+            currentRecords.map((record) => (
+              <tr key={record.id}>
+                <td>{record.id}</td>
+                <td>{record.name}</td>
+                <td>{record.email}</td>
+                <td>{record.role}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="4">Loading...</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button
+          className="pagination-button"
+          onClick={handlePrev}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <span className="pagination-info">{currentPage}</span>
+        <button
+          className="pagination-button"
+          onClick={handleNext}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeList;
